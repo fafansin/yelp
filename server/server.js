b8abfae7d5d5129736c719dbfe470b938e68e321
Laviel <fafansin@gmail.com>
2024-03-07 12:49:31 +0300

    Cleaned server.js a bit

--- server/server.js
+++ server/server.js
@@ -12,25 +12,18 @@ const catchAsync = require('./utils/catchAsync');
 dotenv.config();
 
 mongoose.connect(process.env.DB_HOST, {
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
-    .then(()=>{
-        console.log('Mongodb Connection Success');
-    })
-    .catch(e=>{
-        console.log('Mongodb Connection Failed');
-        console.log(e);
-    })
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(()=>{
+    console.log('Mongodb Connection Success');
+  })
+  .catch(e=>{
+    console.log('Mongodb Connection Failed');
+    console.log(e);
+  })
 app.engine('ejs', ejsMate);
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// app.use(express.urlencoded({extended:true}));
-// app.use(methodOverride('_method'));
-
-// app.use(express.static(__dirname + '/assets'));
 const root = path.resolve(__dirname, '../client', 'build');
 
 app.use(express.static(root));
