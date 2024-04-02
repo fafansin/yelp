afd2f7f493b36941ecc483db2a8b88eafe05e8d2
Laviel <fafansin@gmail.com>
2024-04-02 14:36:10 +0300

    Added the deleted files and the pacjage.json updates

--- server/server.js
+++ server/server.js
@@ -2,79 +2,75 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
-const AppError = require('./AppError');
 
 dotenv.config();
 
 mongoose.connect(process.env.DB_HOST, {
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
-    .then(()=>{
-        console.log('Mongodb Connection Success');
-    })
-    .catch(e=>{
-        console.log('Mongodb Connection Failed');
-        console.log(e);
-    })
-app.engine('ejs', ejsMate);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
-
-app.get('/campgrounds', async (req, res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds', {title: 'Campgrounds', campgrounds});
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 })
-app.get('/campgrounds/new', (req,res)=>{
-    res.render('campgrounds/new');
+.then(()=>{
+    console.log('Mongodb Connection Success');
 })
-app.post('/campgrounds', async (req, res)=>{
-    const {campground} = req.body;
-    const newCamp = new Campground(campground);
-    await newCamp.save();
-    res.redirect('/campgrounds');
-})
-app.delete('/campgrounds/:id', async (req, res)=>{
-    const { id } = req.params;
-    const ref = await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+.catch(e=>{
+    console.log('Mongodb Connection Failed');
+    console.log(e);
 })
 
-app.get('/campgrounds/:id', async (req, res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/show', {campground});
-})
+const root = path.resolve(__dirname, '../client', 'build');
 
-app.put('/campgrounds/:id', async (req,res)=>{
-    const {id} = req.params;
-    const {campground} = req.body;
-    await Campground.findByIdAndUpdate(id, campground, {new:true});
-    res.redirect(`/campgrounds/${id}`);
-})
+app.use(express.static(root));
+app.use(express.json());
 
-app.get('/campgrounds/:id/edit', async (req, res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', {campground});
-})
 
-app.get('/', (req,res) =>{
-    // throw new AppError('Taena this', 400);
-    res.render('home');
+// app.use(express.urlencoded({extended:true}));
+// app.use(methodOverride('_method'));
 
-})
+// app.get('/campgrounds', async (req, res)=>{
+//     const campgrounds = await Campground.find({});
+//     res.render('campgrounds', {title: 'Campgrounds', campgrounds});
+// })
+// app.get('/campgrounds/new', (req,res)=>{
+//     res.render('campgrounds/new');
+// })
+// app.post('/campgrounds', async (req, res)=>{
+//     const {campground} = req.body;
+//     const newCamp = new Campground(campground);
+//     await newCamp.save();
+//     res.redirect('/campgrounds');
+// })
+// app.delete('/campgrounds/:id', async (req, res)=>{
+//     const { id } = req.params;
+//     const ref = await Campground.findByIdAndDelete(id);
+//     res.redirect('/campgrounds');
+// })
 
+// app.get('/campgrounds/:id', async (req, res)=>{
+//     const {id} = req.params;
+//     const campground = await Campground.findById(id);
+//     res.render('campgrounds/show', {campground});
+// })
+
+// app.put('/campgrounds/:id', async (req,res)=>{
+//     const {id} = req.params;
+//     const {campground} = req.body;
+//     await Campground.findByIdAndUpdate(id, campground, {new:true});
+//     res.redirect(`/campgrounds/${id}`);
+// })
+
+// app.get('/campgrounds/:id/edit', async (req, res)=>{
+//     const {id} = req.params;
+//     const campground = await Campground.findById(id);
+//     res.render('campgrounds/edit', {campground});
+// })
+
+// This will will load all urls to main React page
 app.get('*', (req, res) =>{
-    res.render('404');
+  res.sendFile('index.html', {root:root})
+  // res.render('404');
 })
 
 app.use((err, req, res, next) =>{
